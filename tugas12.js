fc35e3ed08768ef12c6e616a38d7f382e59f603d
wiladhianty <wiladhianty@gmail.com>
2021-06-30 13:51:55 +0700

    tambah tugas

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+var buah = ["Pisang" , "jeruk" , "Apel", "Mangga"]
+console.log(buah)
+
+// pop untuk menghapus elemen terakhir
+buah.pop()
+console.log(buah)
+
+// shift untuk menghapus elemn pertama
+buah.shift()
+console.log(buah)
